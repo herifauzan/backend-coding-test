2f4c6d6869a269b6b87ef31de94a372df2f42046
Heri Fauzan <herryfauzan@gmail.com>
2019-08-09 21:08:18 +0700

    nyc testing for coverage > 80%

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const request = require('supertest');
-
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
@@ -26,6 +25,72 @@ describe('API tests', () => {
             request(app)
                 .get('/health')
                 .expect('Content-Type', /text/)
+                .expect(200, done);
+        });
+    });
+
+    describe('GET /rides', () => {
+        it('should return all rides information', (done) => {
+            request(app)
+                .get('/rides')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('GET /rides/1', () => {
+        it('should return rides information with id 1', (done) => {
+            request(app)
+                .get('/rides/1')
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    }); 
+
+    describe('POST /rides', () => {
+        it('should return rides information with id 1', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 0, start_long: 0, end_lat:10, end_long:10, rider_name:'santoso', driver_name:'supri', driver_vehicle:'keranda mayat'})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
+
+    describe('POST /rides', () => {
+        it('should return error with message start latitude or longitude validation error', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: -100, start_long: -100, end_lat:10, end_long:10, rider_name:'santoso', driver_name:'supri', driver_vehicle:'keranda mayat'})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+        it('should return error with message stop latitude or longitude validation error', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 0, start_long: 0, end_lat:-100, end_long:-100, rider_name:'santoso', driver_name:'supri', driver_vehicle:'keranda mayat'})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+        it('should return error with message rider name validation error', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 0, start_long: 0, end_lat:10, end_long:10, rider_name:666, driver_name:'supri', driver_vehicle:'keranda mayat'})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+        it('should return error with message driver name validation error', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 0, start_long: 0, end_lat:10, end_long:10, rider_name:'santoso', driver_name:666, driver_vehicle:'keranda mayat'})
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+        it('should return error with message vehicle name validation error', (done) => {
+            request(app)
+                .post('/rides')
+                .send({start_lat: 0, start_long: 0, end_lat:10, end_long:10, rider_name:'santoso', driver_name:'supri', driver_vehicle:666})
+                .expect('Content-Type', /json/)
                 .expect(200, done);
         });
     });
