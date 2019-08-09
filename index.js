4240ab3f25537067c3e1841c7b9bc2422d126e47
Heri Fauzan <herryfauzan@gmail.com>
2019-08-09 21:58:30 +0700

    add winston

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const port = 8010;
 var cors  = require('cors');
+var log = require('./config/winston');
 
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
@@ -10,8 +11,12 @@ const buildSchemas = require('./src/schemas');
 
 db.serialize(() => {
     buildSchemas(db);
-
-    const app = require('./src/app')(db);
-    app.use(cors());
-    app.listen(port, () => console.log(`App started and listening on port ${port}`));
+    try{
+        const app = require('./src/app')(db);
+        app.use(cors());
+        app.listen(port, () => console.log(`App started and listening on port ${port}`));
+    }
+    catch(e){
+        log.error(e);
+    }
 });
